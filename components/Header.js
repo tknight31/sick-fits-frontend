89aa65629a8d0b5c82c15c000936998a3ef19bd2
Tyler Knight <tknightx31@gmail.com>
2021-06-10 15:33:53 -0400

    header styling

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,50 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
 
 export default function Header() {
   return (
-    <header>
+    <HeaderStyles>
       <div className="bar">
-        <Link href="/">Sick Fits</Link>
-        <div className="sub-bar">
-          <p>search</p>
-        </div>
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
+      </div>
+      <div className="sub-bar">
+        <p>search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyles>
   );
 }
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 10px solid var(--black, black);
+  }
+`;
+
+const Logo = styled.h1`
+  background: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
