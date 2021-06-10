07ca5c64ff76c4fa7c7c968c471a3d887adc2620
Tyler Knight <tknightx31@gmail.com>
2021-06-10 11:48:32 -0400

    Page wrapper config

--- components/Page.js
+++ components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h2>I'm the page comp</h2>
       {children}
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import Page from '../components/Page';
 
 export default function MyApp({ Component, pageProps }) {
