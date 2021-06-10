9b49d2349775ad154255f8ded8e7a72adef0f9f9
Tyler Knight <tknightx31@gmail.com>
2021-06-10 16:05:21 -0400

    nProgress bar implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
