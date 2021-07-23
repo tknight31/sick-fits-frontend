851ac50786dd2bcd3e12b3ede132793fc8de2f1d
Tyler Knight <tknightx31@gmail.com>
2021-07-23 15:05:27 -0400

    Deleting Product

---
+++ components/DeleteProduct.js
@@ -0,0 +1,36 @@
+/* eslint-disable react/prop-types */
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading, error }] = useMutation(
+    DELETE_PRODUCT_MUTATION,
+    {
+      variables: { id },
+    }
+  );
+  return (
+    <button
+      type="button"
+      disabled={loading}
+      onClick={() => {
+        // eslint-disable-next-line no-restricted-globals
+        if (confirm('Are you sure you want to delete this product?')) {
+          console.log('deleting');
+          deleteProduct().catch((err) => alert(err.message));
+        }
+      }}
+    >
+      {children}
+    </button>
+  );
+}
