16b4e71fe172b868ce941144deb04bcc034777eb
Tyler Knight <tknightx31@gmail.com>
2021-07-23 15:10:46 -0400

    evict product from Apollo cache when deleting

--- components/DeleteProduct.js
+++ components/DeleteProduct.js
@@ -11,11 +11,16 @@ const DELETE_PRODUCT_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteProduct));
+}
+
 export default function DeleteProduct({ id, children }) {
   const [deleteProduct, { loading, error }] = useMutation(
     DELETE_PRODUCT_MUTATION,
     {
       variables: { id },
+      update,
     }
   );
   return (
