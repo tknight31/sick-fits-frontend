3fc0af0842deeb5cbecd4527725d995415139c8f
Tyler Knight <tknightx31@gmail.com>
2021-08-02 16:17:25 -0400

    Custom type policies and control over the Apollo cache

--- components/Pagination.js
+++ components/Pagination.js
@@ -7,7 +7,7 @@ import DisplayError from './ErrorMessage';
 import PaginationStyles from './styles/PaginationStyles';
 import { perPage } from '../config';
 
-const PAGINATION_QUERY = gql`
+export const PAGINATION_QUERY = gql`
   query {
     _allProductsMeta {
       count
