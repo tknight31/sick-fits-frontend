59c62ad1539b8bf4b609084717fc27d80aeddade
Tyler Knight <tknightx31@gmail.com>
2021-08-02 15:45:15 -0400

    Dynamic pagination routing and query values

--- components/Products.js
+++ components/Products.js
@@ -1,11 +1,13 @@
+/* eslint-disable react/prop-types */
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import { perPage } from '../config';
 import Product from './Product';
 
 export const ALL_PRODUCTS_QUERY = gql`
-  query ALL_PRODUCTS_QUERY {
-    allProducts {
+  query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int) {
+    allProducts(first: $first, skip: $skip) {
       id
       name
       price
@@ -19,8 +21,13 @@ export const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
-export default function Products() {
-  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+export default function Products({ page }) {
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY, {
+    variables: {
+      skip: page * perPage - perPage,
+      first: perPage,
+    },
+  });
   console.log(data, error, loading);
   if (loading) return <p>Loading....</p>;
   if (error) return <p>Whoooooooops. Error: {error.message}</p>;
